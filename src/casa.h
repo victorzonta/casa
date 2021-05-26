33376f217b728bd5e2a73d8cbe379ef7f057e4e1
victorzonta <zontavlogs77@gmail.com>
2021-05-25 22:32:57 -0300

    first commit

---
+++ src/casa.h
@@ -0,0 +1,110 @@
+char Casa[] PROGMEM = R"=====(
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Casa</title>
+    <style>
+    h1{
+        display: block;
+        text-align: center;
+    }
+    div.cores{
+        display: flex;
+        justify-content: center;
+    }
+    button {
+    width: 150px;
+    height: 100px;
+    font-size: 20px;
+    }
+    #quarto{
+        background-color: #fff;
+    }
+    #quartooff{
+        background-color:#000;
+        color: white;
+    }
+    #garagem{
+        background-color:#fff;
+    }
+    #garagemoff{
+        background-color:#000;
+        color: white;
+    }
+    #escritorio{
+        background-color:#fff;
+    }
+    #escritoriooff{
+        background-color:#000;
+        color: white;
+    }
+    #sala{
+        background-color: #fff;
+    }
+    #salaoff{
+        background-color: #000;
+        color: white;
+    }
+    #casa{
+        background-color: #fff;
+    }
+    #casaoff{
+        background-color: #000;
+        color: white;
+    }
+    </style>
+</head>
+<body>
+    <h1> ligue o comodo da casa </h1>
+    <div class = "cores">      
+        <button id = "quarto">quarto</button>   
+        <button id = "quartooff">quartooff</button>   
+        <button id = "garagem">garagem</button>   
+        <button id = "garagemoff">garagemoff</button>  
+        <button id = "escritorio">escritorio</button>
+        <button id = "escritoriooff">escritoriooff</button>   
+        <button id = "sala">sala</button>       
+        <button id = "salaoff">salaoff</button>   
+        <button id = "casa">casa</button> 
+        <button id = "casaoff">casaoff</button>  
+
+    </div>
+
+    <script>
+        function requisita(botao){
+            let request = new XMLHttpRequest();
+            url = `\\${botao.id}`
+            console.log(url);
+            request.open("POST", url, true);
+            request.send();
+        }
+
+        const quarto = document.getElementById("quarto");
+        btnquarto.addEventListener("click", function(){requisita(btnquarto)});
+        let btnquartooff = document.getElementById("quartooff");
+        btnquartooff.addEventListener("click", function(){requisita(btnquartooff)});
+        let btngaragem = document.getElementById("garagem");
+        btngaragem.addEventListener("click", function(){requisita(btngaragem)});
+        let btngaragemoff= document.getElementById("garagemoff");
+        btngaragemoff.addEventListener("click", function(){requisita(btngaragemoff)});
+        let btnescritorio = document.getElementById("escritorio");
+        btnescritorio.addEventListener("click", function(){requisita(btnescritorio)});
+        let btnescritoriooff = document.getElementById("escritoriooff");
+        btnescritoriooff.addEventListener("click", function(){requisita(btnescritoriooff)});
+        let btnsala = document.getElementById("sala");
+        btnsala.addEventListener("click", function(){requisita(btnsala)});
+        let btnsalaoff = document.getElementById("salaoff");
+        btnsalaoff.addEventListener("click", function(){requisita(btnsalaoff)});
+        let btncasa = document.getElementById("casa");
+        btncasa.addEventListener("click", function(){requisita(btncasa)});
+        let btncasaoff = document.getElementById("casaoff");
+        btncasaoff.addEventListener("click", function(){requisita(btncasaoff)});
+        
+       
+    </script>
+</body>
+</html>
+)=====";
---
+++ src/main.cpp
@@ -0,0 +1,88 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+#include "Casa.h"
+
+#define S D4 
+#define G D2
+#define Q D5
+#define E D6
+
+#define SSID "JUVISRONET"
+#define SENHA "50214514"
+
+ESP8266WebServer servidor;
+
+void quarto(){
+  digitalWrite(Q, HIGH);
+}
+void quartooff(){
+  digitalWrite(Q, LOW);
+}
+void garagem(){
+  digitalWrite(G, HIGH);
+  }
+void garagemoff(){
+  digitalWrite(G, LOW);
+  }
+void escritorio(){
+  digitalWrite(E, HIGH);
+  }
+void escritoriooff(){
+  digitalWrite(E, LOW);
+}
+void sala(){
+  digitalWrite(S, HIGH);
+  }
+void salaoff(){
+  digitalWrite(S, LOW);
+}
+void casa(){
+  digitalWrite(Q, HIGH);
+  digitalWrite(G, HIGH);
+  digitalWrite(E, HIGH);
+  digitalWrite(S, HIGH);
+  }
+void casaoff(){
+  digitalWrite(Q, LOW);
+  digitalWrite(G, LOW);
+  digitalWrite(E, LOW);
+  digitalWrite(S, LOW);
+}
+
+
+void setup() {
+ pinMode(S,OUTPUT);
+ pinMode(G,OUTPUT);
+ pinMode(Q,OUTPUT);
+ pinMode(E,OUTPUT);
+
+ WiFi.begin(SSID,SENHA);
+Serial.begin(115200);
+
+while(WiFi.status()!=WL_CONNECTED)//Enquanto a conexão não é realizada
+{
+  Serial.print(".");
+  delay(500);
+}
+
+Serial.print("IP Address: ");
+Serial.println(WiFi.localIP());//Ip onde tenho que me conectar
+
+servidor.on("/quarto",quarto);
+servidor.on("/quartooff",quartooff);
+servidor.on("/garagem",garagem);
+servidor.on("/garagemoff",garagemoff);
+servidor.on("/escritorio",escritorio);
+servidor.on("/escritoriooff",escritoriooff);
+servidor.on("/sala",sala);
+servidor.on("/salaoff",salaoff);
+servidor.on("/casa",casa);
+servidor.on("/casaoff",casaoff);
+servidor.begin();
+}
+
+void loop() {
+  servidor.send(200,"text/html", Casa);//Imprime a página web no servidor
+  servidor.handleClient();  
+
+}
